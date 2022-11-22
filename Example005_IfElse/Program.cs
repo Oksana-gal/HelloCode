08cafef56e67fbf4124d561f70c62b76770accc0
oksana <galik.oksana@yandex.by>
2022-11-22 16:19:32 +0300

    add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оксана")
+{
+    Console.WriteLine("Привет,это же ОКСАНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
